b6133d6f49bd83b344e3efa16d1d34263f45cd11
Klesman123 <marcos.klesman@gmail.com>
2022-11-16 11:14:44 +0200

    IfAndElse

--- 16-11-2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16-11-2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number !");
+            string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            //teen siia if ja else loogikaga vastused
+            // siin jääb programm seisma ja pead enterit vajutama
+            Console.ReadKey();
+            if (numberParsed >= 1 && numberParsed <= 49)
+            {
+                Console.WriteLine("Hea");
+            }
+            else if (numberParsed >= 50 && numberParsed < 99)
+            {
+                Console.WriteLine("Halb");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
         }
     }
 }
